61cfbd58ae3e812a97936feaf313495df6fac963
tursunov-r <tursunov.rus.developer@gmail.com>
2026-04-20 14:22:46 +0500

    add template.env

--- src/services/order_service.py
+++ src/services/order_service.py
@@ -1,4 +1,4 @@
-from fastapi import status
+from fastapi import status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.schemas.order_schema import (
@@ -8,52 +8,52 @@ from src.schemas.order_schema import (
 
 from src.repositories.order_repository import OrderRepository
 
-order_repo = OrderRepository()
-
 
 class OrderService:
-    @staticmethod
+    def __init__(self, order_repo: OrderRepository = OrderRepository()):
+        self.order_repo = order_repo
+
     async def create_order(
-        new_order: CreateOrderSchema, session: AsyncSession
+        self, new_order: CreateOrderSchema, session: AsyncSession
     ):
-        order = await order_repo.create_order_query(
+        order = await self.order_repo.create_order_query(
             user_id=new_order.user_id,
             products=new_order.product_ids,
             session=session,
         )
         if not order:
-            raise
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+            )
         return order
 
-    @staticmethod
-    async def get_order_by_id(order_id: int, session: AsyncSession):
-        order = await order_repo.get_order_by_id_query(
+    async def get_order_by_id(self, order_id: int, session: AsyncSession):
+        order = await self.order_repo.get_order_by_id_query(
             order_id=order_id, session=session
         )
-        items = await order_repo.get_order_items_query(
+        items = await self.order_repo.get_order_items_query(
             order_id=order_id, session=session
         )
         if not order or not items:
             raise
         return {"order details": order, "order items": items}
 
-    @staticmethod
-    async def update_order(order: UpdateOrderSchema, session: AsyncSession):
-        result = await order_repo.update_order_query(
+    async def update_order(
+        self, order: UpdateOrderSchema, session: AsyncSession
+    ):
+        result = await self.order_repo.update_order_query(
             order_id=order.order_id, status=order.status, session=session
         )
         if not result:
             raise
         return result
 
-    @staticmethod
-    async def get_orders(session: AsyncSession):
-        orders = await order_repo.get_all_orders_query(session=session)
+    async def get_orders(self, session: AsyncSession):
+        orders = await self.order_repo.get_all_orders_query(session=session)
         return orders
 
-    @staticmethod
-    async def get_user_orders(user_id: int, session: AsyncSession):
-        orders = await order_repo.get_user_orders_query(
+    async def get_user_orders(self, user_id: int, session: AsyncSession):
+        orders = await self.order_repo.get_user_orders_query(
             user_id=user_id, session=session
         )
         return orders
